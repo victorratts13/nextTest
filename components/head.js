0a3a235e1450d9f88c98594e33613bd2ab4cf6f5
Victor Ratts <victor.ratts13@gmail.com>
2020-11-12 03:17:35 -0300

    Update head.js

--- components/head.js
+++ components/head.js
@@ -9,7 +9,7 @@ const Head = () => {
                     `
                 }
             </style>
-            <script src="../assets/js/jquery.min.js" ></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" ></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" ></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/3.0.1/socket.io.js" ></script>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" ></link>
